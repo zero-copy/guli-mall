cf8c3e09d58059e8620d822331270f158c6df669
Harlan <isharlan.hu@gmail.com>
2021-10-11 23:37:55 +0800

    feat(mybatisConfig): mybatis 配置

---
+++ mall-common/src/main/java/com/study/common/config/mybatis/MybatisConfig.java
@@ -0,0 +1,35 @@
+package com.study.common.config.mybatis;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Harlan
+ * @date 2021 10 11 23:07
+ */
+@Configuration
+public class MybatisConfig {
+
+    /**
+     * 乐观锁
+     * @return 乐观锁
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+
+    /**
+     * 防止全表操作
+     * @return 防止全表操作
+     */
+    @Bean
+    public BlockAttackInnerInterceptor blockAttackInnerInterceptor() {
+        return new BlockAttackInnerInterceptor();
+    }
+}
