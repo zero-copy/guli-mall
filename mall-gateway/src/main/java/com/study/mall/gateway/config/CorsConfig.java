a438c2c8b35b94aede2d63e55b43e7f228b7fc31
Harlan <isharlan.hu@gmail.com>
2021-10-11 22:43:29 +0800

    feat(init): init

---
+++ mall-gateway/src/main/java/com/study/mall/gateway/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.study.mall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+
+/**
+ * 跨域配置
+ * @author Harlan
+ * @date 2021 10 11 0:50
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        //配置跨域
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        corsConfiguration.setAllowCredentials(true);
+        //所有路径匹配
+        configSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(configSource);
+    }
+}
--- mall-ware/ware-server/src/main/java/com/study/mall/ware/MallWareServerApplication.java
+++ mall-ware/ware-server/src/main/java/com/study/mall/ware/MallWareServerApplication.java
@@ -3,12 +3,14 @@ package com.study.mall.ware;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Harlan
  * @date 2021 10 10 16:36
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan(basePackages = "com.study.mall.ware.mapper")
 public class MallWareServerApplication {
 
