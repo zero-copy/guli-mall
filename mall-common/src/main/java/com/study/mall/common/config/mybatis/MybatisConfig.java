72bfb4cf8d4b14261998870f7fc1af318f09219f
Harlan <isharlan.hu@gmail.com>
2021-10-23 19:18:46 +0800

    feat(MybaistConfig): 添加分页插件

--- mall-common/src/main/java/com/study/mall/common/config/mybatis/MybatisConfig.java
+++ mall-common/src/main/java/com/study/mall/common/config/mybatis/MybatisConfig.java
@@ -1,17 +1,33 @@
 package com.study.mall.common.config.mybatis;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author Harlan
  * @date 2021 10 11 23:07
  */
 @Configuration
+@EnableTransactionManagement
 public class MybatisConfig {
+
+    /**
+     * 分页插件
+     * @return 分页插件
+     */
+    @Bean
+    public PaginationInnerInterceptor paginationInnerInterceptor() {
+        PaginationInnerInterceptor innerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        innerInterceptor.setOverflow(true);
+        innerInterceptor.setMaxLimit(250L);
+        return innerInterceptor;
+    }
 
     /**
      * 乐观锁
