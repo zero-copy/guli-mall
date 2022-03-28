c6124a32efede9c02ce91910d691d429f3e0544d
Harlan <isharlan.hu@gmail.com>
2022-03-28 18:33:35 +0800

    feat(auth-server): 认证服务搭建

---
+++ mall-auth/auth-server/src/main/java/com/study/mall/MallAuthApplication.java
@@ -0,0 +1,21 @@
+package com.study.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Harlan
+ * @date 2022 03 28 下午 05:57
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MallAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAuthApplication.class, args);
+    }
+}
