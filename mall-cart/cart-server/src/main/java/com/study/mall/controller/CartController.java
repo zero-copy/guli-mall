26bbf4ab6aa6c41a6a2158b352b5ed17b7fe50de
Harlan <isharlan.hu@gmail.com>
2022-06-18 16:19:46 +0800

    feat(mall-cart): 添加商品至购物车

--- mall-cart/cart-server/src/main/java/com/study/mall/controller/CartController.java
+++ mall-cart/cart-server/src/main/java/com/study/mall/controller/CartController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -38,9 +39,9 @@ public class CartController {
     }
 
     @GetMapping("/addToCart")
-    public String addToCart(@RequestParam Long skuId, @RequestParam Integer num, Model model) throws ExecutionException, InterruptedException {
+    public String addToCart(@RequestParam Long skuId, @RequestParam Integer num, RedirectAttributes redirectAttributes) throws ExecutionException, InterruptedException {
         cartService.addToCart(skuId, num);
-        model.addAttribute("sukId", skuId);
+        redirectAttributes.addAttribute("skuId", skuId);
         return "redirect:/addToCartSuccess.html";
     }
 
